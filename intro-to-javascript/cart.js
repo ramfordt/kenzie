15e67710dd796591ba8fe292c6a456a2ff3c8b26
ramfordt <ramsy.safadi@gmail.com>
2018-02-08 16:03:55 -0600

    improved exercise with clearer instructions and no query

--- intro-to-javascript/cart.js
+++ intro-to-javascript/cart.js
@@ -1,109 +1,128 @@
-var BANANA_PRICE = 1.00;
-var APPLE_PRICE = 2.00;
-var ORANGE_PRICE = 3.00;
-var SHIPPING_COST = 4.00;
-var TAX_RATE = .05;
+const BANANA_PRICE = 1.00;
+const APPLE_PRICE = 2.00;
+const ORANGE_PRICE = 3.00;
+const SHIPPING_COST = 4.00;
+const TAX_RATE = .05;
 
-var AVAILABLE = "available";
-var OUT_OF_STOCK = "outofstock";
-var LIMITED_SUPPLY = "limitedsupply";
+const OK = "ok";
+const WARNING = "warning";
+const ALERT = "alert";
 
-var availableBananas = 20;
-var availableApples = 20;
-var availableOranges = 3;
+const availableBananas = 20;
+const availableApples = 20;
+const availableOranges = 3;
 
 var selectedBananas = 1;
 var selectedApples = 1;
 var selectedOranges = 1;
 
-var subtotalBananas = calculateProdTotal(BANANA_PRICE, selectedBananas);
-var subtotalApples = calculateProdTotal(APPLE_PRICE, selectedApples);
-var subtotalOranges = calculateProdTotal(ORANGE_PRICE, selectedOranges);
+var subtotalBananas = calculateProductTotal(BANANA_PRICE, selectedBananas);
+var subtotalApples = calculateProductTotal(APPLE_PRICE, selectedApples);
+var subtotalOranges = calculateProductTotal(ORANGE_PRICE, selectedOranges);
 
 var subtotal = 0;
 var shipping = 0;
 var taxes = 0;
 var total = 0;
 
-$(document).ready(function () {
 
-    // initialize product pricing on page
-    $('#bananas .prodPrice').text("$" + BANANA_PRICE.toFixed(2));
-    $('#apples .prodPrice').text("$" + APPLE_PRICE.toFixed(2));
-    $('#oranges .prodPrice').text("$" + ORANGE_PRICE.toFixed(2));
+// go ahead and get the most-accessed elements once and store them in a variable,
+// so we don't have to make jQuery search for the elements multiple times
+// otherwise this become extraordinarily unperformant
+const priceTagBananasElement = document.querySelector('#bananas .prodPrice');
+const priceTagApplesElement = document.querySelector('#apples .prodPrice');
+const priceTagOrangesElement = document.querySelector('#oranges .prodPrice');
+const quantityBananasElement = document.querySelector('#bananas .qty');
+const quantityApplesElement = document.querySelector('#apples .qty');
+const quantityOrangesElement = document.querySelector('#oranges .qty');
+const totalBananasElement = document.querySelector('#bananas > .prodTotal');
+const totalApplesElement = document.querySelector('#apples > .prodTotal');
+const totalOrangesElement = document.querySelector('#oranges > .prodTotal');
+const availabilityBananasElement = document.querySelector('#bananas > .info > .stockStatus');
+const availabilityApplesElement = document.querySelector('#apples > .info > .stockStatus');
+const availabilityOrangesElement = document.querySelector('#oranges > .info > .stockStatus');
+const subtotalElement = document.querySelector('.subtotal');
+const shippingElement = document.querySelector('.shipping');
+const taxesElement = document.querySelector('.taxes');
+const totalElement = document.querySelector('.total');
 
-    // initialize shopping cart with one of each
-    $('#bananas .qty').val(selectedBananas);
-    $('#apples .qty').val(selectedApples);
-    $('#oranges .qty').val(selectedOranges);
+// initialize product pricing on page
+priceTagBananasElement.innerText = "$" + BANANA_PRICE.toFixed(2);
+priceTagApplesElement.innerText = "$" + APPLE_PRICE.toFixed(2);
+priceTagOrangesElement.innerText = "$" + ORANGE_PRICE.toFixed(2);
 
-    // dont let user select more than is available
-    $('#bananas .qty').prop("max", availableBananas);
-    $('#apples .qty').prop("max", availableApples);
-    $('#oranges .qty').prop("max", availableOranges);
+// initialize shopping cart with one of each
+quantityBananasElement.value = selectedBananas;
+quantityApplesElement.value = selectedApples;
+quantityOrangesElement.value = selectedOranges;
 
-    // calculate initial totals
-    updateProdTotal("#bananas", BANANA_PRICE);
-    updateProdTotal("#apples", APPLE_PRICE);
-    updateProdTotal("#oranges", ORANGE_PRICE);
-    updateTotals();
+// dont let user select more than is available
+quantityBananasElement.max = availableBananas;
+quantityApplesElement.max = availableApples;
+quantityOrangesElement.max = availableOranges;
 
-    // calculate at each update
-    $('#bananas .qty').on('change', function() {
-        selectedBananas =  $('#bananas .qty').val();
-        subtotalBananas = calculateProdTotal(BANANA_PRICE, selectedBananas);
-        updateProdTotal("#bananas", subtotalBananas);
-        updateProdAvailability("#bananas", selectedBananas, availableBananas);
-        updateTotals();
-    })
-    $('#apples .qty').on('change', function() {
-        selectedApples =  $('#apples .qty').val();
-        subtotalApples = calculateProdTotal(APPLE_PRICE, selectedApples);
-        updateProdTotal("#apples", subtotalApples);
-        updateProdAvailability("#apples", selectedApples, availableApples);
-        updateTotals();
-    })
-    $('#oranges .qty').on('change', function() {
-        selectedOranges =  $('#oranges .qty').val();
-        subtotalOranges = calculateProdTotal(ORANGE_PRICE, selectedOranges);
-        updateProdTotal("#oranges", subtotalOranges);
-        updateProdAvailability("#oranges", selectedOranges, availableOranges);
-        updateTotals();
-    })
-});
+// calculate initial totals
+updateProductTotal(totalBananasElement, BANANA_PRICE);
+updateProductTotal(totalApplesElement, APPLE_PRICE);
+updateProductTotal(totalOrangesElement, ORANGE_PRICE);
+updateTotals(subtotalElement, shippingElement, taxesElement, totalElement);
 
-function updateProdTotal(id, prodTotal) {
-    $(id+' > .prodTotal').text("$" + prodTotal.toFixed(2));
+// calculate at each update
+quantityBananasElement.onchange = function () {
+    selectedBananas = quantityBananasElement.value;
+    subtotalBananas = calculateProductTotal(BANANA_PRICE, selectedBananas);
+    updateProductTotal(totalBananasElement, subtotalBananas);
+    updateAvailabilityLabel(availabilityBananasElement, selectedBananas, availableBananas);
+    updateTotals(subtotalElement, shippingElement, taxesElement, totalElement);
+}
+quantityApplesElement.onchange = function() {
+    selectedApples = quantityApplesElement.value;
+    subtotalApples = calculateProductTotal(APPLE_PRICE, selectedApples);
+    updateProductTotal(totalApplesElement, subtotalApples);
+    updateAvailabilityLabel(availabilityApplesElement, selectedApples, availableApples);
+    updateTotals(subtotalElement, shippingElement, taxesElement, totalElement);
+}
+quantityOrangesElement.onchange = function() {
+    selectedOranges = quantityOrangesElement.value;
+    subtotalOranges = calculateProductTotal(ORANGE_PRICE, selectedOranges);
+    updateProductTotal(totalOrangesElement, subtotalOranges);
+    updateAvailabilityLabel(availabilityOrangesElement, selectedOranges, availableOranges);
+    updateTotals(subtotalElement, shippingElement, taxesElement, totalElement);
 }
 
-function updateProdAvailabilityLabel(id, label, availability) {
-    $(id+' > .info > .stockStatus').text(label);
-    if (availability == LIMITED_SUPPLY) {
-        $(id+' > .info > .stockStatus').removeClass(AVAILABLE);
-        $(id+' > .info > .stockStatus').addClass(LIMITED_SUPPLY);
-        $(id+' > .info > .stockStatus').removeClass(OUT_OF_STOCK);
-    } else if (availability == OUT_OF_STOCK) {
-        $(id+' > .info > .stockStatus').removeClass(AVAILABLE);
-        $(id+' > .info > .stockStatus').removeClass(LIMITED_SUPPLY);
-        $(id+' > .info > .stockStatus').addClass(OUT_OF_STOCK);
-    } else if (availability == AVAILABLE) {
-        $(id+' > .info > .stockStatus').addClass(AVAILABLE);
-        $(id+' > .info > .stockStatus').removeClass(LIMITED_SUPPLY);
-        $(id+' > .info > .stockStatus').removeClass(OUT_OF_STOCK);
+function updateProductTotal(element, amount) {
+    element.innerText = "$" + amount.toFixed(2);
+}
+
+function updateProdAvailabilityLabel(element, label, severity) {
+    element.innerText = label;
+
+    if (severity === ALERT) {
+        element.classList.add(ALERT);
+        element.classList.remove(OK);
+        element.classList.remove(WARNING);
+
+    } else if (severity === WARNING) {
+        element.classList.add(WARNING);
+        element.classList.remove(OK);
+        element.classList.remove(ALERT);
+
+    } else {
+        element.classList.add(OK);
+        element.classList.remove(WARNING);
+        element.classList.remove(ALERT);
     }
-
 }
 
-function updateTotals() {
-    var prodTotals = [subtotalBananas, subtotalApples, subtotalOranges];
-    subtotal = calculateSubtotal(prodTotals);
+function updateTotals(subtotalElement, shippingElement, taxesElement, totalElement) {
+    subtotal = calculateCartSubtotal(subtotalBananas, subtotalApples, subtotalOranges);
     shipping = calculateShipping();
     taxes = calculateTaxes(subtotal, TAX_RATE);
     total = calculateTotal(subtotal, shipping, taxes);
-    $('.subtotal').text("$" + subtotal.toFixed(2));
-    $('.shipping').text("$" + shipping.toFixed(2));
-    $('.taxes').text("$" + taxes.toFixed(2));
-    $('.total').text("$" + total.toFixed(2));
+    subtotalElement.innerText = "$" + subtotal.toFixed(2);
+    shippingElement.innerText = "$" + shipping.toFixed(2);
+    taxesElement.innerText = "$" + taxes.toFixed(2);
+    totalElement.innerText = "$" + total.toFixed(2);
 }
 
 function calculateShipping () {
--- intro-to-javascript/cart_logic.js
+++ intro-to-javascript/cart_logic.js
@@ -1,66 +1,91 @@
-/* 1. Complete the function below to calculate product line 
-      Input: price of the product (double)
-      Input: quantity selected (integer)
-      Output: product line totals (double)
+/* 1. Complete the function below to calculate product totals 
+      Input: price (double) => price of the product in dollars (i.e., 1.00 for $1.00)
+      Input: quantity (integer) => number of items selected in the cart
+      Output: productTotal (double) = subtotal for that product in dollars
 */
-function calculateProdTotal (price, quantity) {
-    return 0;   // replace this!
+function calculateProductTotal (price, quantity) {
+    return 0; // replace this!
 }
 
-/* 2. Complete the function below to calculate gross cart totals
+/* 2. Complete the function below to calculate gross cart subtotals
       use the array of product line total argument
-      Input: prodTotals is an array of product line totals (array of double)
-      Output: gross cart totals (double)
-  
-      NOTE: since we haven't covered loops yet, please assume the array will
-            always have 3 elements (i.e., bananas, oranges, apples)
+      Input: totalBananas (double) => dollar subtotal for bananas
+      Input: totalApples (double) => dollar subtotal for apples
+      Input: totalOranges (double) => dollar subtotal for oranges
+      Output: cartSubtotal (double) => subtotal for the shopping cart in dollars, gross before taxes and shipping
 */
-function calculateSubtotal (prodTotals) {
-    return 0;   // replace this!
+function calculateCartSubtotal (totalBananas, totalApples, totalOranges) {
+    return 0; // replace this!
 }
 
 /* 3. Complete the function below to calculate added taxes
-      Input: gross cart totals (double) before tax and shipping
-      Input: tax rate (i.e., 0.10 for 10%)
-      Output: added taxes for the cart (double)
+      Input: cartSubtotal (double) => gross before tax and shipping
+      Input: taxRate (double) => i.e., 0.10 for 10%
+      Output: addedTax (double) => added taxes for the cart 
 */
-function calculateTaxes(grossTotal, taxRate) {
-    return 0;   // replace this!
+function calculateTaxes(cartSubtotal, taxRate) {
+    return 0; // replace this!
 }
 
 /* 4. Complete the function below to calculate total
-      Input: gross cart totals (double) before tax and shipping
-      Input: shipping fee (always $4.00)
-      Input: added tax
-      Output: total cart amount inclusive of shipping and taxes (double)
+      Input: cartSubtotal (double) => gross before tax and shipping
+      Input: addedShipping (double) => constant $4.00 surcharge
+      Input: addedTax (double) => added tax for the cart
+      Output: cartTotal = total cart amount inclusive of shipping and taxes (double)
 */
-function calculateTotal (grossTotal, addedShipping, addedTax) {
-    return 0;   // replace this!
+function calculateTotal (cartSubtotal, addedShipping, addedTax) {
+    return 0; // replace this!
 }
 
-/* 5. We want to inform the users when certain items run out of stock
-      Complete the function below to update the label for product availability
-      as users add items to their shopping cart.
-      Input: product is either "banana", "apple", or "orange"
-      Input: selected is the amount selected on the shopping cart
-      Input: availability is the total amount in stock available
-      Output: none
+/* 5. 
+      Our grocery store has limited availability of each product.
 
-      This assignment is a little flexible. Given the combination of number
-      of items selected and the product availability, you can choose what 
-      message you would like to tell the users and under what conditions. 
-      We suggest you tell the users when there is limited supply and when
-      the item is out of stock.
+      We do not want to dissapoint customers by letting them add products to their
+      shopping cart that are no longer available in inventory. 
+
+      You may notice when you first arrive to the shopping cart, next to each product 
+      there is a label with a green message that says "IN STOCK".
+
+      What we want this function to do is update that message as users add items to the cart,
+      depending on how much availability there is left. 
       
-      NOTE: To update the label you need to call the following function:
-                updateProdAvailabilityLabel(product, label, availability)
-            Input: product - pass along the same product argument
-            Input: label - any string you would like as a message
-            Input: availability - one of the following options: 
-                    OUT_OF_STOCK, LIMITED_SUPPLY, AVAILABLE
-            the label will change font color based on selection (green, yellow, red)
+      This function is called each time a user adds or removes items from the shopping cart.
+      This lets us keep the user updated even before they submit an order.
+
+      Input: labelElement (object) => DOM element containing the label. If you are curious, these 
+                are the elements using .stockStatus class in the HTML.
+      Input: amountSelected (integer) => number of items currently in the shopping cart for this product
+      Input: amountAvailable (integer) => number of items currently in stock for this product in the grocery
+
+      NOTE: As opposed to the other functions we have worked on in the shopping cart, you are not expected 
+            to return any value in this function. Therefore there is no outputs. 
+
+      To update the labels, you need to calling the following function, which has already been built:
+
+        updateProdAvailabilityLabel(labelElement, message, severity);
+
+      where
+
+        labelElement (object) => pass along the same labelElement argument
+
+        message (string) => whatever message you want to pass along. The default is "IN STOCK"
+
+        severity => you can define severity of your inventory situtation to make the message more
+            noticeable to users. You have three choices: OK, WARNING, ALERT. Depending on which 
+            argument you choose, your message might be displayed in green, yellow or red. 
+      
+      For example, by calling:
+
+        updateProdAvailabilityLabel(labelElement, "LIMITED SUPPLY", WARNING);
+
+      You will change the message next to the product from "IN STOCK" (or whatever it was before) to 
+      "LIMITED SUPPLY. Furthermore, because you used WARNING it will change the text from green to yellow.
+
+      This assignment is a little flexible. Given the combination of number of items selected and the 
+      product availability, what message would you like to tell your users? When would you like to use the 
+      severity flag to highlight for users?
 
 */
-function updateProdAvailability(product, selected, availability) {
+function updateAvailabilityLabel(labelElement, amountSelected, productAvailability) {
     return; // replace this!
 }
